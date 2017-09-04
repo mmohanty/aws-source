cede6774f94bdd945f3767ef4611ec5e2db6575f
mmohanty001 <manasmohanty@dbs.com>
2017-09-04 15:15:37 +0530

    Changing Lambda

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("America/Los_Angeles");
-    callback(null, {
+    context.done(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
     });
