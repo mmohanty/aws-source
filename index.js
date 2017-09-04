6a0006070bf6150818a72a905e6b3855f271c26b
Manas Mohanty <manasmohanty07@gmail.com>
2017-09-04 23:24:55 +0530

    Modifying index.js for Lambda Serverless Application

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var time = require('time');
-exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
-    context.done(null, {
+exports.handler = function(event, context, callback) {
+    var currentTime = "2017-12-09"; 
+    //currentTime.setTimezone("America/Los_Angeles");
+    callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
     });
-};
+}
